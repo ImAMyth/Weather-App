bfa3b3012270b8358ded10d42dd9a1395b8b5954
ImAMyth <mmaloney31@yahoo.com>
2022-01-09 18:03:07 -0500

    made a switch statement in order to display the proper image and weather condition caption in HTML

--- app.js
+++ app.js
@@ -11,12 +11,46 @@ function todaysWeather() {
             let zip = data.zip
 
             fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${key}`)
-            .then(res => res.json())
-            .then((data) => {
-                let temperature = data.main.temp
-                let weather = data.weather[0].main
-                let weatherImage = document.querySelector(".weatherImage")
-                weatherImage.src = `images/${weather}.svg`
-            })
+                .then(res => res.json())
+                .then((data) => {
+                    let temperature = data.main.temp
+                    let weather = data.weather[0].main
+                    let weatherImage = document.querySelector(".weatherImage")
+                    weatherImage.src = `images/${weather}.svg`
+                    
+                    switch (weather) {
+                        case "Clouds":
+                            weather = "Cloudy"
+                            break;
+
+                        case "Clear":
+                            weather = "Sunny"
+                            break;
+
+                        case "Rain":
+                            weather = "Raining"
+                            break;
+
+                        case "Drizzle":
+                            weather = "Drizzling"
+                            break;
+
+                        case "Snow":
+                            weather = "Snowing"
+                            break;
+
+                        case "Thunderstorms":
+                            weather = "Thunder and Lightning storms"
+
+                        default:
+                            break;
+                    }
+                    loc.innerHTML = `Today's current weather in ${town}, ${state} is ${Math.round((temperature - 273.15) * 9 / 5 + 32)} degrees Fahrenheit and ${weather} outside`
+                })
+
+                .catch((error) => {
+                    console.log(error)
+                })
         })
-    }
+}
+todaysWeather()
