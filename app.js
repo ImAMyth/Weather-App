ce78aac4effb4062ee840ddd531638458d8a00ea
ImAMyth <mmaloney31@yahoo.com>
2022-01-09 21:04:19 -0500

    added a few more weather values to show in HTML

--- app.js
+++ app.js
@@ -15,9 +15,19 @@ function todaysWeather() {
                 .then((data) => {
                     let temperature = data.main.temp
                     let weather = data.weather[0].main
+                    let highTempValue = data.main.temp_max
+                    let lowTempValue = data.main.temp_min
+                    let feelsLikeValue = data.main.feels_like
+                    let humidityValue = data.main.humidity
+                    let windSpeedValue = data.wind.speed
                     let weatherImage = document.querySelector(".weatherImage")
+                    let highTemp = document.querySelector("#highTemp")
+                    let lowTemp = document.querySelector("#lowTemp")
+                    let feelsLike = document.querySelector("#feelsLike")
+                    let humidity = document.querySelector("#humidity")
+                    let windSpeed = document.querySelector("#windSpeed")
                     weatherImage.src = `images/${weather}.svg`
-                    
+
                     switch (weather) {
                         case "Clouds":
                             weather = "Cloudy"
@@ -45,7 +55,12 @@ function todaysWeather() {
                         default:
                             break;
                     }
-                    loc.innerHTML = `Today's current weather in ${town}, ${state} is ${Math.round((temperature - 273.15) * 9 / 5 + 32)} degrees Fahrenheit and ${weather} outside`
+                    loc.innerHTML = `Today's current weather in ${town}, ${state} is ${Math.round((temperature - 273.15) * 9 / 5 + 32)} F and ${weather} outside`
+                    highTemp.innerHTML = `The high temperature for the day is ${Math.round((highTempValue - 273.15) * 9 / 5 + 32)}F`
+                    lowTemp.innerHTML = `The low temperature for the day is ${Math.round((lowTempValue - 273.15) * 9 / 5 + 32)} F`
+                    feelsLike.innerHTML = `Currently feels like ${Math.round((feelsLikeValue - 273.15) * 9 / 5 + 32)} F`
+                    humidity.innerHTML = `Current humidity is ${humidityValue}%`
+                    windSpeed.innerHTML = `Current wind speed is ${Math.round(windSpeedValue * 2.236936)} mph`
                 })
 
                 .catch((error) => {
